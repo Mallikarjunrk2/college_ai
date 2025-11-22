61c9e771647070e5d6ab8e10d7b4a5cfe3ec9383
Mallikarjunrk2 <mallikarjunrk9741@gmail.com>
2025-11-22 08:07:11 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          50: '#f6f5ff',
+          100: '#efeaff',
+          200: '#ddd6fe',
+          300: '#c4b5fd',
+          400: '#a78bfa',
+          500: '#6d28d9', // --brand-500
+          600: '#5b21b6',
+          700: '#4c1d95'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
+      },
+      borderRadius: {
+        '2xl': '1.25rem'
+      }
+    },
   },
   plugins: [],
 };
